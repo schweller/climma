c3f789d4e1255256dfad294a1d6f1c4d12aef6e4
Inácio Schweller <iscmendonca@gmail.com>
2020-03-26 15:14:18 -0300

    Proper basic response for the CLI

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
+import chalk from 'chalk'
 import minimist from 'minimist'
-
-type Foo = {
-  bar: string;
-}
 
 async function main() {
   const argv = minimist(process.argv.slice(2))
   console.log(argv)
 
   if (argv.city === undefined) {
-    throw new Error("City must be provided")
+    console.error(chalk.red('City must be provided!'))
+    throw new Error('City must be provided')
   }
 
-  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${argv.city}&appid=${process.env.API_KEY}`)
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${argv.city}&units=metric&appid=${process.env.API_KEY}`)
 
-  console.log(response.data)
-  
-  console.log("Hello World")
+  console.log(chalk.green(`${argv.city} current temperature is ${response.data.main.temp} celsius`));
 }
 
 main().catch(error => console.log(error))
