d71802239cc86d4ea3a1b74c261bb417bccd2b17
Inácio Schweller <iscmendonca@gmail.com>
2020-03-26 14:56:59 -0300

    base functionality and binary

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
+import axios, { AxiosResponse } from 'axios'
+import minimist from 'minimist'
+
 type Foo = {
   bar: string;
 }
 
-function main() {
+async function main() {
+  const argv = minimist(process.argv.slice(2))
+  console.log(argv)
+
+  if (argv.city === undefined) {
+    throw new Error("City must be provided")
+  }
+
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${argv.city}&appid=${process.env.API_KEY}`)
+
+  console.log(response.data)
+  
   console.log("Hello World")
 }
 
-main()
+main().catch(error => console.log(error))
