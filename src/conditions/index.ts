4c06982891e02a12d33df3f84a2f14ed723d8863
Inácio Schweller <iscmendonca@gmail.com>
2020-03-28 01:34:53 -0300

    all conditions

--- src/conditions/index.ts
+++ src/conditions/index.ts
@@ -30,6 +30,46 @@ const conditions: Conditions = {
   314: "Heavy shower rain and drizzle",
   321: "Shower drizzle",
 
+  // Rain
+  500: "Light rain",
+  501: "Moderate rain",
+  502: "Heavy rain",
+  503: "Very heavy rain",
+  504: "Extreme rain",
+  511: "Freezing rain",
+  520: "Light shower rain",
+  521: "Shower rain",
+  522: "Heavy shower rain",
+  531: "Ragged shower rain",
+
+  // Snow
+  600: "Light snow",
+  601: "Snow",
+  602: "Heavy snow",
+  611: "Sleet",
+  612: "Light shower sleet",
+  613: "Shower sleet",
+  615: "Light rain and snow",
+  616: "Rain and snow",
+  620: "Light shower snow",
+  621: "Shower snow",
+  622: "Heavy shower snow", 
+
+  // Atmosphere
+  701: "Mist",
+  711: "Smoke",
+  721: "Haze",
+  731: "Dust whirls",
+  741: "Fog",
+  751: "Sand",
+  761: "Dust",
+  762: "Ash",
+  771: "Squall",
+  781: "Tornado",
+
+  // Clear
+  800: "Clear sky",
+
   // Clouds
   801: "Few Clouds",
   802: "Scattered Clouds",
