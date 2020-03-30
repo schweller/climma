32365e0f08b936b80b5efa2e4d9db8880d545794
Inácio Schweller <iscmendonca@gmail.com>
2020-03-30 09:06:03 -0300

    render wind direction based on angle

---
+++ src/direction/index.ts
@@ -0,0 +1,15 @@
+
+const directionsMap = ["↓", "↙", "←", "↖", "↑", "↗", "→", "↘"]
+
+/**
+ * Returns a direction unicode character from a degree.
+ * Angle changes at every 22.5 degree (plus .5 to prevent ties). 
+ * Gets the mod 8 to pick a value from directionsMap 
+ * @param angle Degree provided by the API 
+ * @returns 
+ */
+const direction = (angle: number): string => {
+  return directionsMap[(Math.floor((angle / 22.5) + 0.5)) % 8]
+}
+
+export default direction;
