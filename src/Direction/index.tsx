80f7340da50ad0fc7e2a42fd4eb49399f99ed175
Inácio Schweller <iscmendonca@gmail.com>
2020-04-01 17:43:25 -0300

    Prettier on TSX

--- src/Direction/index.tsx
+++ src/Direction/index.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const directionsMap = ["↓", "↙", "←", "↖", "↑", "↗", "→", "↘"]
+const directionsMap = ['↓', '↙', '←', '↖', '↑', '↗', '→', '↘'];
 
 /**
  * Returns a direction unicode character from a degree.
- * Angle changes at every 22.5 degree (plus .5 to prevent ties). 
- * Gets the mod 8 to pick a value from directionsMap 
- * @param angle Degree provided by the API 
- * @returns 
+ * Angle changes at every 22.5 degree (plus .5 to prevent ties).
+ * Gets the mod 8 to pick a value from directionsMap
+ * @param angle Degree provided by the API
+ * @returns
  */
 const getDirectionChar = (angle: number | undefined): string => {
   if (angle === undefined) {
-    return '?'
+    return '?';
   }
-  return directionsMap[(Math.floor((angle / 22.5) + 0.5)) % 8]
-}
+  return directionsMap[Math.floor(angle / 22.5 + 0.5) % 8];
+};
 
-const Direction = ({deg}: { deg?: number}) => {
-  const char = getDirectionChar(deg)
-  
-  return <>{char}</>
-}
+const Direction = ({ deg }: { deg?: number }) => {
+  const char = getDirectionChar(deg);
+
+  return <>{char}</>;
+};
 
 export default Direction;
--- src/conditions/ascii.tsx
+++ src/conditions/ascii.tsx
@@ -1,46 +1,70 @@
-import React from 'react'
-import { Box, Color } from 'ink'
+import React from 'react';
+import { Box, Color } from 'ink';
 
-const Ascii = ({id}: {id?: number}) => {
+const Ascii = ({ id }: { id?: number }) => {
   switch (id) {
     // clear
     case 800:
       return (
         <Box flexDirection="column">
-          <Box><Color yellow>{"  \\   /  "}</Color></Box>
-          <Box><Color yellowBright>{"   .-.   "}</Color></Box>
-          <Box><Color yellowBright>{"- (   ) -"}</Color></Box>
-          <Box><Color yellowBright>{"   `-᾿   "}</Color></Box>
-          <Box><Color yellow>{"  /   \\ "}</Color></Box>        
+          <Box>
+            <Color yellow>{'  \\   /  '}</Color>
+          </Box>
+          <Box>
+            <Color yellowBright>{'   .-.   '}</Color>
+          </Box>
+          <Box>
+            <Color yellowBright>{'- (   ) -'}</Color>
+          </Box>
+          <Box>
+            <Color yellowBright>{'   `-᾿   '}</Color>
+          </Box>
+          <Box>
+            <Color yellow>{'  /   \\ '}</Color>
+          </Box>
         </Box>
-      )
+      );
     // partial clouds
-    case 801:  
+    case 801:
     case 802:
       return (
         <Box flexDirection="column">
-          <Box><Color yellow>{"  \\_      "}</Color></Box>
-          <Box><Color yellow>{`_/ .-.     `}</Color></Box>
-          <Box><Color cyan>{` \\(   ).  `}</Color></Box>
-          <Box><Color cyan>{` (___(__)  `}</Color></Box>
-          <Box>{"           "}</Box>
+          <Box>
+            <Color yellow>{'  \\_      '}</Color>
+          </Box>
+          <Box>
+            <Color yellow>{`_/ .-.     `}</Color>
+          </Box>
+          <Box>
+            <Color cyan>{` \\(   ).  `}</Color>
+          </Box>
+          <Box>
+            <Color cyan>{` (___(__)  `}</Color>
+          </Box>
+          <Box>{'           '}</Box>
         </Box>
-      )
+      );
     // very cloudy
     case 803:
     case 804:
       return (
         <Box flexDirection="column">
-          <Box>{"           "}</Box>
-          <Box><Color cyan>{`   _.--.   `}</Color></Box>
-          <Box><Color cyan>{` .(_(   ᾿) `}</Color></Box>
-          <Box><Color cyan>{`(__.__)__᾿)`}</Color></Box>
-          <Box>{"            "}</Box>          
+          <Box>{'           '}</Box>
+          <Box>
+            <Color cyan>{`   _.--.   `}</Color>
+          </Box>
+          <Box>
+            <Color cyan>{` .(_(   ᾿) `}</Color>
+          </Box>
+          <Box>
+            <Color cyan>{`(__.__)__᾿)`}</Color>
+          </Box>
+          <Box>{'            '}</Box>
         </Box>
-      )
+      );
     default:
-      return <Box>No weather ASCII available</Box>
+      return <Box>No weather ASCII available</Box>;
   }
-}
+};
 
-export default Ascii
+export default Ascii;
